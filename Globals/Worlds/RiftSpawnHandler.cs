435de24d6c072f26e4ee07edb7013a2209b6885e
DiamondWalker <19913710+DiamondWalker@users.noreply.github.com>
2026-01-02 18:18:45 -0500

    Return portal

--- Globals/Worlds/RiftSpawnHandler.cs
+++ Globals/Worlds/RiftSpawnHandler.cs
@@ -37,30 +37,40 @@ namespace AAMod.Globals.World {
         }
 
         public override void PostUpdate() {
-            if (Main.netMode != 1 && AAWorld.downedEquinox && !VoidSubworld.IsInside()) {
-                SpawnDeactivatedZero();
+            if (Main.netMode != 1) {
+                if (VoidSubworld.IsInside()) {
+                    Point spawnTilePos = new Point(Main.spawnTileX, Main.spawnTileY - 5);
+                    Vector2 spawnPos = new Vector2(spawnTilePos.X * 16, spawnTilePos.Y * 16);
+                    bool anyRiftsExist = NPC.AnyNPCs(mod.NPCType("VoidRift"));
+                    if (!anyRiftsExist) {
+                        int whoAmI = NPC.NewNPC((int)spawnPos.X, (int)spawnPos.Y, ModContent.NPCType<VoidRift>());
+                        RiftX = (int)spawnPos.X;
+                        RiftY = (int)spawnPos.Y;
+                        if (Main.netMode == 2 && whoAmI != -1 && whoAmI < 200) {
+                            NetMessage.SendData(MessageID.SyncNPC, number: whoAmI);
+                        }
+                    }
+                } else {
+                    int VoidHeight = 140;
+
+                    Point spawnTilePos = new Point((Main.maxTilesX / 15 * 14) + (Main.maxTilesX / 15 / 2) - 100, VoidHeight);
+                    Vector2 spawnPos = new Vector2(spawnTilePos.X * 16, spawnTilePos.Y * 16);
+                    bool anyRiftsExist = NPC.AnyNPCs(mod.NPCType("VoidRift"));
+                    if (!anyRiftsExist) {
+                        int whoAmI = NPC.NewNPC((int)spawnPos.X, (int)spawnPos.Y, ModContent.NPCType<VoidRift>());
+                        RiftX = (int)spawnPos.X;
+                        RiftY = (int)spawnPos.Y;
+                        if (Main.netMode == 2 && whoAmI != -1 && whoAmI < 200) {
+                            NetMessage.SendData(MessageID.SyncNPC, number: whoAmI);
+                        }
+                    }
+                }
             }
         }
 
         public void Reset() {
             RiftX = -1;
             RiftY = -1;
-        }
-
-        public void SpawnDeactivatedZero() {
-            int VoidHeight = 140;
-
-            Point spawnTilePos = new Point((Main.maxTilesX / 15 * 14) + (Main.maxTilesX / 15 / 2) - 100, VoidHeight);
-            Vector2 spawnPos = new Vector2(spawnTilePos.X * 16, spawnTilePos.Y * 16);
-            bool anyRiftsExist = NPC.AnyNPCs(mod.NPCType("VoidRift"));
-            if (!anyRiftsExist) {
-                int whoAmI = NPC.NewNPC((int)spawnPos.X, (int)spawnPos.Y, ModContent.NPCType<VoidRift>());
-                RiftX = (int)spawnPos.X;
-                RiftY = (int)spawnPos.Y;
-                if (Main.netMode == 2 && whoAmI != -1 && whoAmI < 200) {
-                    NetMessage.SendData(MessageID.SyncNPC, number: whoAmI);
-                }
-            }
         }
     }
 }
